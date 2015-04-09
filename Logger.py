6ff3631781aee9e24e9e6d812aaff04ca8efdb35
Flkalas <rectifying@gmail.com>
2015-04-09 18:31:34 +0900

    로거 작성중.
    헤헤...

--- Logger.py
+++ Logger.py
@@ -1,3 +1,4 @@
+import os
 
 class LOGGER(object):
     def __init__(self):
@@ -5,21 +6,34 @@ class LOGGER(object):
     
     def initLogger(self,pathBasic="./results/"):
         self.pathBasic = pathBasic
-        self.numSimulation = 0
-        self.numBlock = -1
-        self.numGeneration = 0
-        
+        self.findPrefixNumber()
+
+    def writePrefixFile(self):
+        fileOpened = open(self.pathBasic + str(self.prefix) + ".chk",'w')
+        fileOpened.close()
+        pass
+            
     def findPrefixNumber(self):
+        i = 0
+        while not os.path.isfile(self.pathBasic + str(i) + ".chk"):
+            i += 1
+        
+        self.prefix = i        
+        self.writePrefixFile()
+        
+        return self.prefix
+    
+    def getFileName(self):
         pass
     
     def writeTrainPercent(self,numSimulation,numGeneration,percent,numBlock=-1):
         pass
             
-    def writeDiversity(self,numBlock=-1):
-        pass
-    
-    def writeTestPercent(self,numSimulation,numBlock=-1):
+    def writeDiversity(self,numSimulation,numGeneration,content,numBlock=-1):
         pass
 
     def writeStructure(self,numSimulation,numBlock=-1):
+        pass
+    
+    def writeTestPercent(self,numSimulation,numBlock=-1):
         pass
